20c79ec1939ec8e024f6208a9f2804ae6eeca924
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-07-22 18:06:36 +0530

    Time: 45 ms (58.00%), Space: 13.9 MB (67.86%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,9 +1,8 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        partialans= pow(x,n-1)
-        if n ==0:
+        temp = pow(x,n-1)
+        if n==0:
             return 1
-        
         else:
-            return x*partialans
+            return x*temp
             
