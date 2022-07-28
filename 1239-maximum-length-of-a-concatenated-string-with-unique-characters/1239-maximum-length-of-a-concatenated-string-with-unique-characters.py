c1ed3c66d0f633ee6431d135dcbc0dd9bb344506
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-07-29 00:07:00 +0530

    Time: 172 ms (53.26%), Space: 13.9 MB (89.51%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -2,6 +2,8 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
         charset = set()
         
+        
+#Overlap function is our helper function to finding out that the occurence of char is not more than 1
         def overlap(charset, s):
             # c = Counter(charset)+counter(s)
             # return max(c.values())>1
@@ -17,6 +19,9 @@ class Solution:
                 return len(charset)
             
             res=0
+
+#if condition for appending the char and making sure by the overlap function that it doesnot contain nay duplicates
+
             if not overlap(charset,arr[i]):
                 for c in arr[i]:
                     charset.add(c)
