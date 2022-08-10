594440b0715d319bc88b487f37c6a0f2094c9262
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-08-11 00:00:48 +0530

    Time: 82 ms (50.13%), Space: 17.5 MB (32.92%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -7,13 +7,15 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow, entry, fast = head, head, head
+        
         if head == None or head.next is None:
             return None
+        
         while fast.next!= None and fast.next.next!=None:
             slow = slow.next
             fast = fast.next.next
-            if slow== fast:
-                while slow!=entry:
+            if slow== fast:  #There is a cycle
+                while slow!=entry:  #Found the entry location
                     slow = slow.next
                     entry = entry.next
                 return entry
