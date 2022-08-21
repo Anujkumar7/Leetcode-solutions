57ed5e44d9b2129a7cfa314cca12deec573b90fe
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-08-22 00:46:21 +0530

    Time: 147 ms (5.03%), Space: 18.6 MB (90.57%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -5,22 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def height(self,root):
-        if root ==None:
-            return 0
-        return 1+ max(self.height(root.left), self.height(root.right))
-    
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root == None:
-            return True
-        lh = self.height(root.left)
-        rh = self.height(root.right)
-        if lh - rh >1 or rh - lh >1:
-            return False
+#MORE OPTIMISED SOLUTION -> O(N)
+  # Height function:
+        def height(self,root):
+            if root ==None:
+                return 0
+            return 1+ max(self.height(root.left), self.height(root.right))
+        def isBalanced(self, root: Optional[TreeNode]) -> bool:
+            if root == None:
+                return True
+            lh = self.height(root.left)
+            rh = self.height(root.right)
+            if lh - rh >1 or rh - lh >1:
+                return False
         
-        isLeftBalanced = self.isBalanced(root.left)
-        isRightBalanced = self.isBalanced(root.right)
-        if isLeftBalanced and isRightBalanced:
-            return True
-        else:
-            return False
+            isLeftBalanced = self.isBalanced(root.left)
+            isRightBalanced = self.isBalanced(root.right)
+            if isLeftBalanced and isRightBalanced:
+                return True
+            else:
+                return False
