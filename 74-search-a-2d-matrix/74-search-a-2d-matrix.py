0e1198d2de4b2268db034131f06e23867183e615
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-09-05 16:29:19 +0530

    Time: 70 ms (50.89%), Space: 14.4 MB (43.34%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,13 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix, target: int) -> bool:
+        
+    #BINARY SEARCH:    
+    
         if not matrix or target is None:
             return False
 
         rows, cols = len(matrix), len(matrix[0])
-        low, high = 0, rows * cols - 1
+        low, high = 0, rows * cols - 1  #low ,end == start end==  start =0, end = [(row*col)-1]== last element of the matrix
         
         while low <= high:
             mid = (low + high) // 2
+    
+ #For accessing a element in a matrix, we need 2 things row inex and col index
+#For row index = index no. / total no. of cols
+#For col index = index % total no. of cols
             num = matrix[mid // cols] [mid % cols]
             
             #Implementing Binary search
