28da070e9f6afcdf5fdee550edcbe0e79832b802
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-09-05 23:32:58 +0530

    Time: 78 ms (12.14%), Space: 14 MB (38.36%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,8 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        j=0
-        for i in range(m,m+n):
-            nums1[i]=nums2[j]
-            j+=1
-        nums1.sort()
+       #last index nums[1] 
+        m, n,last = m-1, n-1, m+n-1 
+        
+      #Start merging from reverse order  
+        while n>=0 and m>=0:
+            if nums1[m]> nums2[n]:
+                nums1[last] = nums1[m]
+                m-=1
+            else:
+                nums1[last]= nums2[n]
+                n-=1
+            last-=1
+         #For edge case: Fill nums[1] with leftover nums[2] elements 
+        if n >-1:
+            nums1[0:n+1] = nums2[0:n+1]
+                
+        
+        
+        
+        
+        # j=0
+        # for i in range(m,m+n):
+        #     nums1[i]=nums2[j]
+        #     j+=1
+        # nums1.sort()
