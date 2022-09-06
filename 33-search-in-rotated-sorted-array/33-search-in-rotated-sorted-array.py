0ae8cb3c120ddea07d58ed5efa5ad9a8cc2f6da2
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-09-07 00:19:11 +0530

    Time: 77 ms (26.95%), Space: 14.2 MB (90.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,7 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-         if target not in nums:
-            return -1
-         else:
-            a=nums.index(target)
-            return a
+        
+  #Optimised solution
+
+        left , right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = (left+ right)//2
+            if target== nums[mid]:
+                return mid
+            
+            #Left sorted portion
+            if nums[left]<= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left =  mid+1
+                else:
+                    right =  mid - 1
+                    
+            #Right sorted portion
+            else:
+                if target< nums[mid] or target > nums[right] :
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                    
+        return -1
+        
+      #Linear traversal algo  TC: O(n)  and SC: 0(1) 
+    
+            # if target not in nums:
+            #     return -1
+            # else:
+            #     a=nums.index(target)
+            #     return a
