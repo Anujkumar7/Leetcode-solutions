779d1179240fcd80d2f078659f17fc1c3ce05c34
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-09-13 23:40:30 +0530

    Time: 771 ms (92.45%), Space: 28 MB (40.00%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         summ = 0
         maxx = -10**4
-        for i in range(0,len(nums)):
+        for i in range(len(nums)):
             summ += nums[i]
             maxx = max(summ,maxx)
             if summ <0:
