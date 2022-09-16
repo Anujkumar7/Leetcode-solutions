8ee53c33a45c31b9b3f5f16c7e708d40879ad9fa
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-09-16 17:58:33 +0530

    Time: 2518 ms (40.28%), Space: 13.8 MB (99.83%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        
-        if total % 2 == 1:
-            return False
-        target = total//2
-        
-        dp = [False]*(target+1)
+    # equal partition check
+        if total % 2 != 0:
+            return False 
+        target = total // 2
+    
+        dp = [False]*(target + 1)
         dp[0] = True
-        
+    
         for num in nums:
-            for val in range(target,num-1,-1):
-                dp[val] = dp[val] or dp[val-num]
-        
-        return dp[-1]
-        
+            for i in range(target, num-1, -1):
+                if dp[target]: return True
+                dp[i] = dp[i] or dp[i-num]
+        return dp[target]
