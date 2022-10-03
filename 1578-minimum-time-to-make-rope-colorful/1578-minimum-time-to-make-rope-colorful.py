72ecb45237c35d5391503c8f967e5ab155a028d5
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-10-03 15:46:00 +0530

    Time: 2074 ms (48.27%), Space: 24.7 MB (82.67%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -3,12 +3,18 @@ class Solution:
         prev = 0
         ans = 0
         for i in range(1, len(colors)):
+            
+            # If the prev index character == current character 
             if (colors[i] == colors[prev]):
+                
+#Then compare the needed time of both if the needed time of prev is less than the current one add that into ans
                 if (neededTime[prev]< neededTime[i]):
                     ans+= neededTime[prev]
                     prev = i
+#Else update the ans as the current char needed time
                 else:
                     ans+= neededTime[i]
+#If the prev char != currrent char then update the prev variable with the curr i           
             else:
                 prev = i
         return ans
