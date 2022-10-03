442ab5eb04ed3f1728bad30d28f8556efcaaf41a
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-10-03 15:06:51 +0530

    Time: 222 ms (30.05%), Space: 15.8 MB (54.53%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        #Bottom up approach
+        
         n = len(grid)
         m = len(grid[0])
         dp = [[0]* m for i in range(n)]
@@ -10,14 +12,18 @@ class Solution:
                 else:
                     up = grid[i][j]
                     left = grid[i][j]
-                    if i> 0:
+                    
+                    if i> 0: #if i is out of bound
                         up+= dp[i-1][j]
                     else:
                         up+= sys.maxsize
-                    if j>0:
+                    
+                    if j>0: # if j is out of bound
                         left+= dp[i][j-1]
                     else:
                         left+= sys.maxsize
+                        
+                        #return the min of up and left
                     dp[i][j] =  min(up, left)
         return dp[n-1][m-1]
                     
