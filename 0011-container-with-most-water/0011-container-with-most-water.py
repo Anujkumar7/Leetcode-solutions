1340ada3605d6183f5f5417632bc20f0296fceff
Anujkumar7 <Kumaranuj2k1@gmail.com>
2022-11-02 16:19:17 +0530

    Time: 796 ms (90.93%), Space: 27.5 MB (44.60%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #BRUTE FORCE
+        
+#         res = 0
+#         for L in range(len(height)):
+#             for r in range(L+1, len(height)):
+                
+#                 #area of a rectangle
+#                 area = (r- L) * min(height[L], height[r])
+#                 res = max(area, res)
+#         return res
+    
+    #Optimised soln
+        res = 0
+        l,r = 0, len(height)-1
+        while l< r:
+            area = (r- l) * min(height[l], height[r])
+            res = max(res, area)
+            
+            if height[l]< height[r]:
+                l+=1
+            else:
+                r-=1
+        return res
+        
